e53ccd6ad64cd6962b5efeb545ea5194d3937eb8
Yukiko Noda <yukiko8318@gmail.com>
2023-11-10 14:08:42 +0900

    fix top news

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+// News open/close function
+const moreButtonEl = document.querySelector('#service > .news > .more');
+const moreListEl = document.querySelector('#service > .news > .logdata > .more-list');
+moreButtonEl.addEventListener('click', () => {
+  const isOpen = moreButtonEl.classList.toggle('open');
+  moreButtonEl.textContent = isOpen ? 'close' : 'open';
+  moreListEl.classList.toggle('open');
+})
