e279cf973904806a3ee87f418ad32a91a8de3b52
Yukiko Noda <yukiko8318@gmail.com>
2024-03-11 16:47:11 +0900

    refactor download.js

--- static/js/download.js
+++ static/js/download.js
@@ -1,11 +1,86 @@
+import {
+  causalGeneColumns,
+  geneticTestingColumns,
+  phenotypesJaColumns,
+  phenotypesEnColumns,
+  bioResourceCellColumns,
+  bioResourceMouseColumns,
+  bioResourceDnaColumns,
+  variantClinvarColumns,
+} from './paginationColumns.js';
+
 export const downloadDatasets = (nandoId, datasets) => {
-  function prepareDataToDownload(format, datasets) {
+  function prepareDataToDownload(format) {
     if (format === 'json') {
       return JSON.stringify(prepareJsonData(), null, 2);
     } else if (format === 'txt') {
       return prepareTxtData();
     }
     return '';
+  }
+
+  function prepareDatasets() {
+    const convertedDatasets = Object.values(datasets)
+      .map(({ name, data }) => {
+        switch (name) {
+          case 'Overview':
+            return { name, data };
+          case 'Causal Genes':
+            return { name, data: reconstructionData(causalGeneColumns, data) };
+          case 'Genetic Testing':
+            return {
+              name,
+              data: reconstructionData(geneticTestingColumns, data),
+            };
+          case 'Phenotypes':
+            const currentLang =
+              document.querySelector('.language-select').value;
+            const currentColumns =
+              currentLang === 'ja' ? phenotypesJaColumns : phenotypesEnColumns;
+            return { name, data: reconstructionData(currentColumns, data) };
+          case 'Cell':
+            return {
+              name,
+              data: reconstructionData(bioResourceCellColumns, data),
+            };
+          case 'Mouse':
+            return {
+              name,
+              data: reconstructionData(bioResourceMouseColumns, data),
+            };
+          case 'DNA':
+            return {
+              name,
+              data: reconstructionData(bioResourceDnaColumns, data),
+            };
+          case 'Clinvar':
+            return {
+              name,
+              data: reconstructionData(variantClinvarColumns, data),
+            };
+          case 'MGeND':
+            return;
+        }
+      })
+
+    return convertedDatasets;
+  }
+
+  function reconstructionData(columns, data) {
+    return data.map((d) => {
+      const reducedData = {};
+      for (const [currentKey, currentValue] of Object.entries(d)) {
+        const column = columns.find((column) => column.id === currentKey);
+        if (column) {
+          reducedData[column.label] = currentValue;
+        }
+      }
+      const orderedData = {};
+      for (const column of columns) {
+        orderedData[column.label] = reducedData[column.label];
+      }
+      return orderedData;
+    });
   }
 
   function prepareJsonData() {
@@ -22,7 +97,7 @@ export const downloadDatasets = (nandoId, datasets) => {
       Object.keys(categoryMappings).map((category) => [category, {}])
     );
     //カテゴリーに分類
-    datasets.forEach(({ name, data }) => {
+    prepareDatasets().forEach(({ name, data }) => {
       const category = Object.keys(categoryMappings).find((category) =>
         categoryMappings[category].includes(name)
       );
@@ -81,7 +156,6 @@ export const downloadDatasets = (nandoId, datasets) => {
       if (Array.isArray(categoryData)) {
         txtData += `-- ${categoryName} --\n`;
         if (categoryData.length > 0) {
-          console.log(categoryName,Object.keys(categoryData[0]))
           const keysTxt = Object.keys(categoryData[0]).join('\t') + '\n';
           const valuesTxt = categoryData
             .map((item) => Object.values(item).join('\t'))
@@ -93,7 +167,6 @@ export const downloadDatasets = (nandoId, datasets) => {
         Object.entries(categoryData).forEach(([key, value]) => {
           txtData += `-- ${categoryName}/${key} --\n`;
           if (value.length > 0) {
-            console.log(key,Object.keys(value[0]))
             const keysTxt = Object.keys(value[0]).join('\t') + '\n';
             const valuesTxt = value
               .map((item) => Object.values(item).join('\t'))
@@ -126,8 +199,7 @@ export const downloadDatasets = (nandoId, datasets) => {
     .addEventListener('submit', function (e) {
       e.preventDefault();
       const format = new FormData(this).get('format');
-      const dataToDownload = prepareDataToDownload(format, datasets);
-      console.log(dataToDownload);
+      const dataToDownload = prepareDataToDownload(format);
 
       // データをダウンロード用ファイルとしてユーザーに提供
       const mimeType = format === 'json' ? 'application/json' : 'text/plain';
