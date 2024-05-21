238f4c23ac065cbd58c8ed3f07b8bbb522ff4dc7
行事光 <h.gyoji.vrds@gmail.com>
2024-05-21 16:56:04 +0900

    add smart text box

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,23 @@
 import { navToggle } from './navigation.js';
 import { focusInput } from './focusInput.js';
-import { changePlaceholder } from './changePlaceholder.js';
 import { setLangChange } from './setLangChange.js';
 import { drawEpidemiologyTable } from './epidemiology.js';
+import { smartTextBox } from './smart_text_box.js';
 
 navToggle();
 focusInput();
-changePlaceholder();
 setLangChange();
 if (window.location.pathname === '/epidemiology') {
   drawEpidemiologyTable();
 }
+
+// smart text box
+
+smartTextBox('NANDO', '/static/tsv/NANDO.tsv', {
+  api_url: '',
+});
+
+document.addEventListener('selectedLabel', function (event) {
+  const labelInfo = event.detail.labelInfo;
+  console.log('object', labelInfo);
+});
