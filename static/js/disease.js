444ac78279704b1a2e8b70cb0da1319555e17589
行事光 <h.gyoji.vrds@gmail.com>
2024-10-18 13:06:55 +0900

    add sub class logic

--- static/js/disease.js
+++ static/js/disease.js
@@ -70,15 +70,19 @@ const datasets = [
         makeInheritanceUris(entryData);
         makeLinkedItem(entryData);
         addChartToDiv(entryData);
-        addPaginationTableToDiv(entryData);
+        addContentToDiv(entryData);
         checkSummaryData(entryData);
         makeDiseaseDefinition(entryData);
         updateOverviewLinkAndContentDisplay();
-        const chartTypeSelect = document.getElementById(
+        const numOfPatientsSelect = document.getElementById(
           'num-of-patients-graph'
         );
-        chartTypeSelect.addEventListener('change', function () {
+        numOfPatientsSelect.addEventListener('change', function () {
           addChartToDiv(entryData);
+        });
+        const subClassSelect = document.getElementById('sub-class-graph');
+        subClassSelect.addEventListener('change', function () {
+          addContentToDiv(entryData);
         });
         datasets.find((d) => d.name === 'Overview').data = entryData;
         checkAndLogDatasets();
@@ -761,58 +765,99 @@ function addChartToDiv(entryData) {
 }
 
 // TODO: Subclass
-function addPaginationTableToDiv(entryData) {
+function addContentToDiv(entryData) {
   const targetDiv = document.getElementById('temp-sub-class');
-
+  const chartTypeSelect = document.getElementById('sub-class-graph');
+  const selectedChartType = chartTypeSelect ? chartTypeSelect.value : 'table';
   if (targetDiv) {
-    // togostanza-pagination-table要素を作成
-    const tableElement = document.createElement('togostanza-pagination-table');
-    tableElement.setAttribute(
-      'data-url',
-      `https://dev-nanbyodata.dbcls.jp/sparqlist/api/test_get_nandoID?nando_id=${entryData.nando_id}`
-    );
-    tableElement.setAttribute('data-type', 'json');
-    tableElement.setAttribute('data-unavailable_message', 'No data found.');
-    tableElement.setAttribute('custom-css-url', '');
-    tableElement.setAttribute('width', '');
-    tableElement.setAttribute('fixed-columns', '1');
-    tableElement.setAttribute('padding', '0px');
-    tableElement.setAttribute('page-size-option', '100');
-    tableElement.setAttribute('page-slider', 'false');
-    tableElement.setAttribute(
-      'columns',
-      `[{
-        "id": "label",
-        "label": "Subclass(JA)",
-        "escape": false,
-        "line-clamp": 2
-      },
-      {
-        "id": "engLabel",
-        "label": "Subclass(En)",
-        "escape": false,
-        "line-clamp": 2
-      },
-      {
-        "id": "id",
-        "label": "Notification Number",
-        "escape": true,
-        "line-clamp": 1,
-        "link": "idurl",
-        "target": "_blank"
-      }]`
-    );
+    targetDiv.innerHTML = ''; // 既存の内容をクリア
 
-    // スクリプト要素を動的に作成して挿入
-    const scriptElement = document.createElement('script');
-    scriptElement.type = 'module';
-    scriptElement.src =
-      'https://togostanza.github.io/metastanza/pagination-table.js';
-    scriptElement.async = true;
+    if (selectedChartType === 'table') {
+      // Tableを表示
+      const tableElement = document.createElement(
+        'togostanza-pagination-table'
+      );
+      tableElement.setAttribute(
+        'data-url',
+        `https://dev-nanbyodata.dbcls.jp/sparqlist/api/test_get_nandoID?nando_id=${entryData.nando_id}`
+      );
+      tableElement.setAttribute('data-type', 'json');
+      tableElement.setAttribute('data-unavailable_message', 'No data found.');
+      tableElement.setAttribute('custom-css-url', '');
+      tableElement.setAttribute('width', '');
+      tableElement.setAttribute('fixed-columns', '1');
+      tableElement.setAttribute('padding', '0px');
+      tableElement.setAttribute('page-size-option', '100');
+      tableElement.setAttribute('page-slider', 'false');
+      tableElement.setAttribute(
+        'columns',
+        `[{
+          "id": "label",
+          "label": "Subclass(JA)",
+          "escape": false,
+          "line-clamp": 2
+        },
+        {
+          "id": "engLabel",
+          "label": "Subclass(En)",
+          "escape": false,
+          "line-clamp": 2
+        },
+        {
+          "id": "id",
+          "label": "Notification Number",
+          "escape": true,
+          "line-clamp": 1,
+          "link": "idurl",
+          "target": "_blank"
+        }]`
+      );
 
-    // targetDivにテーブルとスクリプトを追加
-    targetDiv.appendChild(tableElement);
-    targetDiv.appendChild(scriptElement);
+      // スクリプト要素を動的に作成して挿入
+      const scriptElement = document.createElement('script');
+      scriptElement.type = 'module';
+      scriptElement.src =
+        'https://togostanza.github.io/metastanza/pagination-table.js';
+      scriptElement.async = true;
+
+      // targetDivにテーブルとスクリプトを追加
+      targetDiv.appendChild(tableElement);
+      targetDiv.appendChild(scriptElement);
+    } else if (selectedChartType === 'tree') {
+      // Treeを表示
+      const treeElement = document.createElement('togostanza-tree');
+      treeElement.setAttribute(
+        'data-url',
+        `https://dev-nanbyodata.dbcls.jp/sparqlist/api/test_get_nandoID?nando_id=${entryData.nando_id}`
+      );
+      treeElement.setAttribute('data-type', 'json');
+      treeElement.setAttribute('sort-key', 'id');
+      treeElement.setAttribute('sort-order', 'ascending');
+      treeElement.setAttribute('graph-layout', 'horizontal');
+      treeElement.setAttribute('node-label-key', 'id');
+      treeElement.setAttribute('node-label-margin', '8');
+      treeElement.setAttribute('node-size-key', 'size');
+      treeElement.setAttribute('node-size-min', '8');
+      treeElement.setAttribute('node-size-max', '8');
+      treeElement.setAttribute('node-color-key', 'color');
+      treeElement.setAttribute('node-color-group', 'group');
+      treeElement.setAttribute('node-color-blend', 'normal');
+      treeElement.setAttribute('tooltips-key', 'name');
+      treeElement.setAttribute('togostanza-custom_css_url', '');
+
+      treeElement.style.setProperty('--togostanza-canvas-height', '600px');
+
+      // スクリプト要素を動的に作成して挿入
+      const scriptElement = document.createElement('script');
+      scriptElement.type = 'module';
+      scriptElement.src =
+        'https://togostanza.github.io/metastanza-devel/tree.js';
+      scriptElement.async = true;
+
+      // targetDivにツリーとスクリプトを追加
+      targetDiv.appendChild(treeElement);
+      targetDiv.appendChild(scriptElement);
+    }
   }
 }
 
