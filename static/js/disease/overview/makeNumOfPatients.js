35588310b879a192d98a2f2f991f159acf9853e2
行事光 <h.gyoji.vrds@gmail.com>
2024-10-28 17:31:35 +0900

    use innerHTML

--- static/js/disease/overview/makeNumOfPatients.js
+++ static/js/disease/overview/makeNumOfPatients.js
@@ -4,101 +4,101 @@ export function makeNumOfPatients(entryData) {
   const selectedChartType = chartTypeSelect ? chartTypeSelect.value : 'line';
 
   if (targetDiv) {
-    targetDiv.innerHTML = ''; // Clear existing chart
+    // Clear existing chart and script
+    targetDiv.innerHTML = '';
 
-    let chartElement;
-    if (selectedChartType === 'bar') {
-      // Create Bar Chart
-      chartElement = document.createElement('togostanza-barchart');
-      chartElement.setAttribute(
-        'data-url',
-        `https://dev-nanbyodata.dbcls.jp/sparqlist/api/takatsuki_test_20240322?nando_id=${entryData.nando_id}`
-      );
-      chartElement.setAttribute('data-type', 'json');
-      chartElement.setAttribute('category', 'year');
-      chartElement.setAttribute('value', 'num_of_patients');
-      chartElement.setAttribute('category-title', 'Year');
-      chartElement.setAttribute('value-title', 'Num of Patients');
-      chartElement.setAttribute('chart-type', 'stacked');
-      chartElement.setAttribute('width', '900');
-      chartElement.setAttribute('height', '600');
-      chartElement.setAttribute('legend', 'true');
-      chartElement.setAttribute('xaxis-placement', 'bottom');
-      chartElement.setAttribute('yaxis-placement', 'left');
-      chartElement.setAttribute('xlabel-padding', '10');
-      chartElement.setAttribute('ylabel-padding', '5');
-      chartElement.setAttribute('xlabel-alignment', 'center');
-      chartElement.setAttribute('ylabel-alignment', 'right');
+    const chartSrc =
+      selectedChartType === 'bar'
+        ? 'https://togostanza.github.io/metastanza/barchart.js'
+        : 'https://togostanza.github.io/metastanza-devel/linechart.js';
 
-      chartElement.setAttribute('padding-inner', '.8');
-      chartElement.setAttribute('padding-outer', '.5');
-      chartElement.setAttribute('bar-width', '0.8');
-      chartElement.setAttribute('legend-title', 'Categories');
-      chartElement.setAttribute('xgrid', 'false');
-      chartElement.setAttribute('ygrid', 'true');
-      chartElement.setAttribute('xtick', 'false');
-      chartElement.setAttribute('ytick', 'true');
-      chartElement.setAttribute('xlabel-max-width', '200');
-      chartElement.setAttribute('ylabel-max-width', '200');
-      chartElement.setAttribute('xlabel-alignment', 'center');
-      chartElement.setAttribute('ylabel-alignment', 'right');
-      // Apply custom styles
-      chartElement.style.setProperty('--togostanza-series-0-color', '#29697a');
-      chartElement.style.setProperty('--togostanza-label-font-size', '14');
-    } else {
-      // Create Line Chart
-      chartElement = document.createElement('togostanza-linechart');
-      chartElement.setAttribute(
-        'data-url',
-        `https://dev-nanbyodata.dbcls.jp/sparqlist/api/takatsuki_test_20240322?nando_id=${entryData.nando_id}`
-      );
-      chartElement.setAttribute('data-type', 'json');
-      chartElement.setAttribute('axis-x-key', 'year');
-      chartElement.setAttribute('axis-x-scale', 'ordinal');
-      chartElement.setAttribute('axis-x-placement', 'bottom');
-      chartElement.setAttribute('axis-x-title', 'Year');
-      chartElement.setAttribute('axis-x-title_padding', '40');
-      chartElement.setAttribute('axis-x-ticks_label_angle', '0');
-      chartElement.setAttribute('axis-y-key', 'num_of_patients');
-      chartElement.setAttribute('axis-y-scale', 'linear');
-      chartElement.setAttribute('axis-y-placement', 'left');
-      chartElement.setAttribute('axis-y-title', 'Num of Patients');
-      chartElement.setAttribute('axis-y-title_padding', '50');
-      chartElement.setAttribute('axis-y-ticks_label_angle', '0');
-      chartElement.setAttribute('point_size', '10');
-      chartElement.setAttribute('legend-title', 'Year');
-      chartElement.setAttribute('tooltips-key', 'num_of_patients');
-      chartElement.setAttribute('grouping-key', 'group');
-      chartElement.style.setProperty(
-        '--togostanza-theme-series_0_color',
-        '#29697a'
-      );
-      chartElement.style.setProperty(
-        '-togostanza-fonts-font_size_primary',
-        '14'
-      );
-      chartElement.style.setProperty(
-        '--togostanza-fonts-font_size_secondary',
-        '14'
-      );
+    const chartHtml = `
+      <${
+        selectedChartType === 'bar'
+          ? 'togostanza-barchart'
+          : 'togostanza-linechart'
+      }
+        data-url="https://dev-nanbyodata.dbcls.jp/sparqlist/api/takatsuki_test_20240322?nando_id=${
+          entryData.nando_id
+        }"
+        ${
+          selectedChartType === 'bar'
+            ? `
+            data-type="json"
+            category="year"
+            value="num_of_patients"
+            category-title="Year"
+            value-title="Num of Patients"
+            chart-type="stacked"
+            width="900"
+            height="400"
+            legend="true"
+            xaxis-placement="bottom"
+            yaxis-placement="left"
+            xlabel-padding="10"
+            ylabel-padding="5"
+            xlabel-alignment="center"
+            ylabel-alignment="right"
+            padding-inner=".8"
+            padding-outer=".5"
+            bar-width="0.8"
+            legend-title="Categories"
+            xgrid="false"
+            ygrid="true"
+            xtick="false"
+            ytick="true"
+            xlabel-max-width="200"
+            ylabel-max-width="200"
+          `
+            : `
+            data-type="json"
+            axis-x-key="year"
+            axis-x-scale="ordinal"
+            axis-x-placement="bottom"
+            axis-x-title="Year"
+            axis-x-title_padding="40"
+            axis-x-ticks_label_angle="0"
+            axis-y-key="num_of_patients"
+            axis-y-scale="linear"
+            axis-y-placement="left"
+            axis-y-title="Num of Patients"
+            axis-y-title_padding="50"
+            axis-y-ticks_label_angle="0"
+            point_size="10"
+            legend-title="Year"
+            tooltips-key="num_of_patients"
+            grouping-key="group"
+          `
+        }
+        style="
+          --togostanza-series-0-color: #29697a;
+          --togostanza-label-font-size: 14;
+          ${
+            selectedChartType === 'line'
+              ? `
+            --togostanza-canvas-height: 400;
+            --togostanza-canvas-width: 1000;
+            --togostanza-fonts-font_size_primary: 14;
+            --togostanza-fonts-font_size_secondary: 14;
+          `
+              : ''
+          }
+      "></${
+        selectedChartType === 'bar'
+          ? 'togostanza-barchart'
+          : 'togostanza-linechart'
+      }>
+    `;
 
-      // Apply custom styles for line chart
-      chartElement.style.setProperty('--togostanza-canvas-height', '600');
-      chartElement.style.setProperty('--togostanza-canvas-width', '1000');
-    }
+    targetDiv.innerHTML = chartHtml;
 
     // Create and add the script dynamically
     const scriptElement = document.createElement('script');
     scriptElement.type = 'module';
-    if (selectedChartType === 'bar') {
-      scriptElement.src = `https://togostanza.github.io/metastanza/barchart.js`;
-    } else {
-      scriptElement.src = `https://togostanza.github.io/metastanza-devel/linechart.js`;
-    }
+    scriptElement.src = chartSrc;
     scriptElement.async = true;
 
-    // Add chart and script to targetDiv
-    targetDiv.appendChild(chartElement);
+    // Add the script to the targetDiv
     targetDiv.appendChild(scriptElement);
   }
 }
