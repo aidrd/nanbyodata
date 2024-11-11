6f96df4d5f95277df4d23dc7d59fc87d995431a2
行事光 <h.gyoji.vrds@gmail.com>
2024-11-11 11:07:02 +0900

    add navigation to disease page

--- static/js/utils/navigation.js
+++ static/js/utils/navigation.js
@@ -32,3 +32,45 @@ export const navToggle = () => {
     }, 350);
   }
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdown = document.getElementById('dropdownMenu');
+  const icon = document.getElementById('dropdownIcon');
+
+  // Toggle dropdown visibility and icon
+  function toggleDropdown(event) {
+    event.stopPropagation();
+    const isDropdownHidden = dropdown.style.display === 'none';
+    dropdown.style.display = isDropdownHidden ? 'block' : 'none';
+    icon.classList.toggle('fa-angle-down', !isDropdownHidden);
+    icon.classList.toggle('fa-angle-up', isDropdownHidden);
+
+    document.querySelector(".nav-link[href='javascript:void(0);']").blur();
+  }
+
+  // Add event listener to the "DATA" nav link
+  document
+    .querySelector(".nav-link[href='javascript:void(0);']")
+    .addEventListener('click', toggleDropdown);
+
+  // Close dropdown when clicking outside
+  document.addEventListener('click', function (event) {
+    if (
+      !event.target.closest('.dropdown') &&
+      dropdown.style.display === 'block'
+    ) {
+      dropdown.style.display = 'none';
+      icon.classList.add('fa-angle-down');
+      icon.classList.remove('fa-angle-up');
+    }
+  });
+
+  // Close dropdown when clicking a dropdown item
+  document.querySelectorAll('#dropdownMenu .dropdown-item').forEach((item) => {
+    item.addEventListener('click', () => {
+      dropdown.style.display = 'none';
+      icon.classList.add('fa-angle-down');
+      icon.classList.remove('fa-angle-up');
+    });
+  });
+});
