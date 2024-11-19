fad2fe013c58e651981be795b468f9f1c1870d14
fujitoyo <fujiwara.toyofumi@gmail.com>
2024-11-19 16:09:22 +0900

    update the NANDO file for SmartBox

--- static/js/disease.js
+++ static/js/disease.js
@@ -541,7 +541,7 @@ function trySwitchingContent(hash, retries = 0) {
 
 // smart box
 
-smartBox('NANDO', '/static/tsv/NANDO_20240516.tsv', {
+smartBox('NANDO', '/static/tsv/NANDO_20241023.tsv', {
   api_url: '',
 });
 
